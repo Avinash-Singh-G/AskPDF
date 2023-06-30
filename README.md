5529c621fb4894aff2bc6de16ed713507dac5262
Avinash-Singh-DL <avinash.singh@dharmalifelabs.com>
2023-06-30 22:31:42 +0530

    app.py + sample doc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ### AskPDF
+
+## Sample Output on Terminal
+Enter the file path: oops.pdf
+Enter the query (Press 'q' to stop): what is a constructor?
+
+ A constructor is a special method whose name is the same as the class name. The constructors serve the special purpose of initializing the objects.
+
+Enter the query (Press 'q' to stop): what are the oops principles?
+
+ The principles of Object-Oriented Programming (OOP) are encapsulation, abstraction, inheritance, and polymorphism. Encapsulation is the process of combining data and functions into a single unit, or object. Abstraction is the process of hiding the details of an object and only showing the essential features. Inheritance is the process of creating a new class from an existing class. Polymorphism is the ability of an object to take on multiple forms.
+
+Enter the query (Press 'q' to stop): q
---
+++ app.py
@@ -0,0 +1,41 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import pinecone
+import os
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', 'your-openai-api-key')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY', 'your-pinecone-api-key')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV', 'your-pinecone-api-env')
+
+# 1. Load the document
+file_path = input("Enter the file path: ")
+loader = PyPDFLoader(file_path)
+data = loader.load()
+
+# 2. Chunk the document
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0)
+texts = text_splitter.split_documents(data)
+
+# 3. Create Embeddings Vector Store
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+pinecone.init(
+    api_key=PINECONE_API_KEY,  
+    environment=PINECONE_API_ENV  
+)
+index_name = "askpdf"
+vector = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+
+# 4. Query the pdf
+llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+chain = load_qa_chain(llm, chain_type="stuff")
+while True:
+    query = input("Enter the query (Press 'q' to stop): ")
+    if query == 'q':
+        break
+    docs = vector.similarity_search(query)
+    res=chain.run(input_documents=docs, question=query)
+    print(res)
